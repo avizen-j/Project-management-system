3555883c58d4300f2dd7ea543bde176caf5a772a
avizen-j <avizen.j@gmail.com>
2020-05-18 23:28:39 +0300

    Changes to DB.

--- project-management-system/Context/Assignment.cs
+++ project-management-system/Context/Assignment.cs
@@ -7,13 +7,15 @@ using System.Threading.Tasks;
 
 namespace project_management_system.Context
 {
-    public class User
+    public class Assignment
     {
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
-        public int UserID { get; set; }
-        public DateTime RegistrationDate { get; set; }
-        public string Username { get; set; }
-        public string Password { get; set; }
+        public int AssignmentID { get; set; }
+        public string AssignmentName { get; set; }
+        public string AssignmentDescription { get; set; }
+        public string Priority { get; set; }
+        public string Status { get; set; }
+
         public IList<AssignmentUser> AssignmentUsers { get; set; }
     }
 }
---
+++ project-management-system/Context/AssignmentUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace project_management_system.Context
+{
+    public class AssignmentUser
+    {
+        public int UserID { get; set; }
+        public User User { get; set; }
+
+        public int AssignmentID { get; set; }
+        public Assignment Assignment { get; set; }
+    }
+}
